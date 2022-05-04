d3e992dc8c2d2b96d9e9fb6ae750c1c05bdb9d53
qwerty7989 <koonyoch@hotmail.com>
2022-05-04 19:54:23 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('SaltyHouse-Database', 'void', 'cWjWNaqk9LtyZ6jB',{
-    host: 'saltyhouse.database.windows.net',
+const db = new Sequelize('saltyhouse-database', 'void', 'cWjWNaqk9LtyZ6jB',{
+    host: 'korkuefood.database.windows.net',
     dialect: 'mssql',
     define: {
         timestamps: false,
         freezeTableName: true
     },
     logging: false,
-    schema: 'vip'
+    schema: 'dbo'
 });
 
 module.exports = db;
